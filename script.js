f6627c0865769557a35e58fb235caac947057413
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-03-28 16:42:04 +0200

    add body

---
+++ script.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(1460, 800)
+}
+function draw() {
+    background("lightblue");
+    myDreamHouse();
+}
+function myDreamHouse() {
+    //Body
+    fill("pink");
+    stroke("black");
+    rect(550, 400, 450, 400);
+
+}
